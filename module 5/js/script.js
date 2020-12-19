272b66717b04f316450aa7ddf71516689eb2d0f1
insularius <sultan.sekenov@yandex.ru>
2020-12-19 20:26:32 +0600

    дд

--- module 5/js/script.js
+++ module 5/js/script.js
@@ -1,29 +1,49 @@
-// DOM manipulation
-// console.log(document.getElementById("title"));
-// console.log(document instanceof HTMLDocument);
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    function sayHello (event) {
+      console.log(event);
 
-function sayHello () {
-  var name =
-   document.getElementById("name").value;
-   var message = "<h2>Hello " + name + "!</h2>";
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
 
-  // document
-  //   .getElementById("content")
-  //   .textContent = message;
-
-  document
-    .getElementById("content")
-    .innerHTML = message;
-
-  if (name === "student") {
-    var title = 
       document
-        .querySelector("#title")
-        .textContent;
-    title += " & Lovin' it!";
-    document
-        .querySelector("h1")
-        .textContent = title;
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
+    document.querySelector("body")
+      .addEventListener("mousemove",
+        function (event) {
+          if (event.shiftKey === true) {
+            console.log("x: " + event.clientX);
+            console.log("y: " + event.clientY);
+          }
+        }
+      );
+
   }
-}
-© 2020 GitHub, Inc.
+);
+
+
+
+// document.querySelector("button")
+//   .onclick = sayHello;
