2e6d687ba90d3da79948b0a7b0c29879af7989b8
insularius <sultan.sekenov@yandex.ru>
2020-12-20 16:37:07 +0600

    kk

--- module 5/js/script.js
+++ module 5/js/script.js
@@ -2,48 +2,21 @@
 document.addEventListener("DOMContentLoaded",
   function (event) {
     
-    function sayHello (event) {
-      console.log(event);
-
-      this.textContent = "Said it!";
-      var name =
-       document.getElementById("name").value;
-       var message = "<h2>Hello " + name + "!</h2>";
-
-      document
-        .getElementById("content")
-        .innerHTML = message;
-
-      if (name === "student") {
-        var title = 
-          document
-            .querySelector("#title")
-            .textContent;
-        title += " & Lovin' it!";
-        document
-            .querySelector("h1")
-            .textContent = title;
-      }
-    }
-
     // Unobtrusive event binding
     document.querySelector("button")
-      .addEventListener("click", sayHello);
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-    document.querySelector("body")
-      .addEventListener("mousemove",
-        function (event) {
-          if (event.shiftKey === true) {
-            console.log("x: " + event.clientX);
-            console.log("y: " + event.clientY);
-          }
-        }
-      );
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
 
+        
+      });
   }
 );
-
-
-
-// document.querySelector("button")
-//   .onclick = sayHello;
