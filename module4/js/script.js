38e795c57a4556b364d090b3442c0cadd5a9e7e0
insularius <sultan.sekenov@yandex.ru>
2021-04-09 20:16:50 +0600

    i

--- module4/js/script.js
+++ module4/js/script.js
@@ -199,7 +199,7 @@ console.log(b);
 
 
 
-
+//48
 // Pass by reference vs by value
 function changePrimitive(primValue) {
   console.log("in changePrimitive...");
@@ -234,7 +234,7 @@ console.log("after changeObject, orig value:");
 console.log(value);
 
 
-
+//49
 // Function constructors
 function Circle (radius) {
   this.radius = radius;
@@ -272,3 +272,75 @@ var literalCircle = {
 };
 
 console.log(literalCircle.getArea());
+
+//50
+// Arrays
+var array = new Array();
+array[0] = "Yaakov";
+array[1] = 2;
+array[2] = function (name) {
+  console.log("Hello " + name);
+};
+array[3] = {course: " HTML, CSS & JS"};
+
+console.log(array);
+array[2](array[0]);
+console.log(array[3].course);
+
+
+// Short hand array creation
+var names = ["Yaakov", "John", "Joe"];
+console.log(names);
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}
+
+names[100] = "Jim";
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}
+
+var names2 = ["Yaakov", "John", "Joe"];
+
+var myObj = {
+  name: "Yaakov",
+  course: "HTML/CSS/JS",
+  platform: "Courera"
+};
+for (var prop in myObj) {
+  console.log(prop + ": " + myObj[prop]);
+}
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+names2.greeting = "Hi!";
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+
+//51
+// Closures
+function makeMultiplier (multiplier) {
+  // var multiplier = 2;
+  function b() {
+    console.log("Multiplier is: " + multiplier);
+  }
+  b();
+
+
+  return (
+      function (x) {
+        return multiplier * x;
+      }
+
+    );
+}
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10)); // its own exec env
+
